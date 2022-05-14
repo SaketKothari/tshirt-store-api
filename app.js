0ff059398c8059d7215381e2ebbfdd155827c7f5
SaketKothari <saketkothari722@gmail.com>
2022-05-14 11:15:42 +0530

    📝 Injecting docs and middleware

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+// for swagger documentation
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//cookies and file middleware
+app.use(cookieParser());
+app.use(fileUpload());
+
+// morgan middleware
+app.use(morgan('tiny'));
 
 // import all routes here
 const home = require('./routes/home');
