a3b5204e0c140953f57224258bd9507c467ba4f3
SaketKothari <saketkothari722@gmail.com>
2022-05-14 11:34:23 +0530

    🔥 The big promise

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,13 +1,21 @@
-exports.home = (req, res) => {
+const BigPromise = require('../middlewares/bigPromise');
+
+exports.home = BigPromise(async (req, res) => {
+  // const db = await something()
   res.status(200).json({
     success: true,
     greeting: 'Hello from API',
   });
-};
+});
 
-exports.homeDummy = (req, res) => {
-  res.status(200).json({
-    success: true,
-    greeting: 'This is another dummy route',
-  });
+exports.homeDummy = async (req, res) => {
+  try {
+    // const db = await something()
+    res.status(200).json({
+      success: true,
+      greeting: 'This is another dummy route',
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
