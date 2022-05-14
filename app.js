3db027cb720762143c2fc2bc9527d3957d77d17e
SaketKothari <saketkothari722@gmail.com>
2022-05-14 10:57:38 +0530

    Routes and Controllers demo

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 require('dotenv').config();
-
 const app = express();
+
+// import all routes here
+const home = require('./routes/home');
+
+// router middleware
+app.use('/api/v1', home);
 
 // export app.js
 module.exports = app;
