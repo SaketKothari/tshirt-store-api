84048794280523ca693fe2c001e5a606d9abbd73
SaketKothari <saketkothari722@gmail.com>
2022-05-25 12:34:52 +0530

    Aggregation filter in where clause

--- utils/whereClause.js
+++ utils/whereClause.js
@@ -24,6 +24,27 @@ class WhereClause {
     return this;
   }
 
+  filter() {
+    const copyQ = { ...this.bigQ };
+
+    delete copyQ['search'];
+    delete copyQ['limit'];
+    delete copyQ['page'];
+
+    // convert bigQ into a string => copyQ
+    let stringOfCopyQ = JSON.stringify(copyQ);
+
+    stringOfCopyQ = stringOfCopyQ.replace(
+      /\b(gte|lte|gt|lt)\b/g,
+      (m) => `$${m}`
+    );
+
+    const jsonOfCopyQ = JSON.parse(stringOfCopyQ);
+
+    this.base = this.base.find(jsonOfCopyQ);
+    return this;
+  }
+
   pager(resultperPage) {
     let currentPage = 1;
     if (this.bigQ.page) {
