8d98e4c7cb42a1a002f42bdaf1eeff95bcf3c7b5
SaketKothari <saketkothari722@gmail.com>
2022-05-30 23:01:08 +0530

    Setup payment routes

--- app.js
+++ app.js
@@ -34,11 +34,13 @@ app.use(morgan('tiny'));
 const home = require('./routes/home');
 const user = require('./routes/user');
 const product = require('./routes/product');
+const payment = require('./routes/payment');
 
 // router middleware
 app.use('/api/v1', home);
 app.use('/api/v1', user);
 app.use('/api/v1', product);
+app.use('/api/v1', payment);
 
 app.get('/signuptest', (req, res) => {
   res.render('signuptest');
