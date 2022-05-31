769027df9983b3be87ba451f69a6f73be9e6f12b
SaketKothari <saketkothari722@gmail.com>
2022-05-31 13:11:57 +0530

    Delete order

--- routes/order.js
+++ routes/order.js
@@ -5,9 +5,10 @@ const {
   getLoggedInOrders,
   admingetAllOrders,
   adminUpdateOrder,
+  adminDeleteOrder,
 } = require('../controllers/orderController');
 const router = express.Router();
-const { isLoggedIn } = require('../middlewares/user');
+const { isLoggedIn, customRole } = require('../middlewares/user');
 
 router.route('/order/create').post(isLoggedIn, createOrder);
 router.route('/order/:id').get(isLoggedIn, getOneOrder);
@@ -19,6 +20,7 @@ router
   .get(isLoggedIn, customRole('admin'), admingetAllOrders);
 router
   .route('/admin/order/:id')
-  .put(isLoggedIn, customRole('admin'), adminUpdateOrder);
+  .put(isLoggedIn, customRole('admin'), adminUpdateOrder)
+  .delete(isLoggedIn, customRole('admin'), adminDeleteOrder);
 
 module.exports = router;
